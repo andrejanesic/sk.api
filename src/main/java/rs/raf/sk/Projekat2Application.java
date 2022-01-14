369ce2fbbcb193edf63763e461975e54511c4c22
Andreja Nesic <office@andrejanesic.com>
2022-01-14 23:03:38 +0100

    initial commmit

---
+++ src/main/java/rs/raf/sk/Projekat2Application.java
@@ -0,0 +1,13 @@
+package rs.raf.sk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekat2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projekat2Application.class, args);
+	}
+
+}
---
+++ src/test/java/rs/raf/sk/Projekat2ApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.raf.sk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projekat2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
